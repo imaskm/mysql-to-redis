e895507f15e580508238d3e1052b2a27ad726668
Ashwani <ashwanisharma686@gmail.com>
2020-02-25 11:18:13 +0530

    First commit

---
+++ convert.py
@@ -0,0 +1,26 @@
+import pymysql,redis
+
+mysql_user = "root"
+mysql_password = "root12345"
+redis_password = ""
+#change value of other variables like host, port and db_name according to your usecase
+conn = pymysql.connect( host='0.0.0.0',port=3307,user=mysql_user,
+                        password=mysql_password,db='db_name',charset='utf8mb4',
+                        cursorclass=pymysql.cursors.DictCursor)
+
+sql = 'select  word,definition from db_table '
+
+cursor = conn.cursor()
+
+cursor.execute(sql)
+
+result = cursor.fetchall()
+conn.close()
+d={}
+
+for el in result:
+    d[  el["word"].lower()  ] = el["definition"].lower()
+
+
+r = redis.Redis( host='localhost',port=6379,password=redis_pwd)
+r.close()
