535bedad8f5543e9aab89ae507574e6918238054
Ashwani <ashwanisharma686@gmail.com>
2020-03-16 17:25:57 +0530

    Update convert.py
    added set command

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ import pymysql,redis
 
 mysql_user = "root"
 mysql_password = "root12345"
-redis_password = ""
+redis_password = "12345"
 #change value of other variables like host, port and db_name according to your usecase
 conn = pymysql.connect( host='0.0.0.0',port=3307,user=mysql_user,
                         password=mysql_password,db='db_name',charset='utf8mb4',
@@ -22,5 +22,6 @@ for el in result:
     d[  el["word"].lower()  ] = el["definition"].lower()
 
 
-r = redis.Redis( host='localhost',port=6379,password=redis_pwd)
+r = redis.Redis( host='localhost',port=6379,password=redis_password)
+r.mset(d)
 r.close()
